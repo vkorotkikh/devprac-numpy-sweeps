e471b20300ab8772cda299635ee74ca1074d83f6
vkorotkikh <va.korotki@gmail.com>
2017-08-02 19:33:24 -0400

    add rwh_primes2 from stackoverflow

--- eratosthenes_sieve.py
+++ eratosthenes_sieve.py
@@ -10,12 +10,13 @@
 # ******************************************************************************
 # Begin Imports
 import math
+import itertools
 import numpy as np
 
 
 def main():
 	m_sieve()
-
+	pres = rwh_primes2_python3(100)
 
 def m_sieve():
 	is_prime 		= np.ones((100,), dtype=bool)
@@ -30,6 +31,25 @@ def m_sieve():
 	for i in range(0, len(primes)):
 		print(primes[i])
 
+def rwh_primes2_python3(n):
+	izip = itertools.zip_longest
+	chain = itertools.chain.from_iterable
+	compress = itertools.compress
+    """ Input n>=6, Returns a list of primes, 2 <= p < n """
+    zero = bytearray([False])
+    size = n//3 + (n % 6 == 2)
+    sieve = bytearray([True]) * size
+    sieve[0] = False
+    for i in range(int(n**0.5)//3+1):
+      if sieve[i]:
+        k=3*i+1|1
+        start = (k*k+4*k-2*k*(i&1))//3
+        sieve[(k*k)//3::2*k]=zero*((size - (k*k)//3 - 1) // (2 * k) + 1)
+        sieve[  start ::2*k]=zero*((size -   start  - 1) // (2 * k) + 1)
+    ans = [2,3]
+    poss = chain(izip(*[range(i, n, 6) for i in (1,5)]))
+    ans.extend(compress(poss, sieve))
+    return ans
 
 if __name__ == "__main__":
 	main()
